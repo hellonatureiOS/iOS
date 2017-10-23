7799e2dec1c00b0ef0cb172cfd83909e934d16a3
hellonature <app@hellonature.net>
2017-10-23 12:22:24 +0900

    added SwiftyGif

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'hellonature' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   	use_frameworks!
   	# Pods for hellonature
-	pod ‘Firebase/Core’
-	pod ‘Firebase/Messaging’
+	pod 'Firebase/Core'
+	pod 'Firebase/Messaging'
+    pod 'SwiftyGif'
 end
--- hellonature/ViewController.swift
+++ hellonature/ViewController.swift
@@ -7,11 +7,12 @@ import WebKit
 import Firebase
 import FirebaseInstanceID
 import FirebaseMessaging
+import SwifyGif
 
 
 let SITE_DOMAIN:String = "http://www.hellonature.net/mobile_shop/"
 let SITE_PARAMETER:String = "UserScreen=iphone_app&hwid="
-let SITE_BANNER:String = "http://www.hellonature.net/mobile_dev/app/index.html"
+let SITE_BANNER:String = "http://www.hellonature.net/mobile_shop/app/index.html"
 let LOADED_APP_BANNER:String = "loaded app banner"
 let CLOSE_APP_BANNER:String = "close app banner"
 
@@ -19,6 +20,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
     
     var webView: WKWebView!
     var banner: WKWebView!
+    var splash: UIImageView!
     var webViewStarted:Bool = false
     var showStatusBar:Bool = false
     
@@ -26,6 +28,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
     override func viewDidLoad() {
  
         super.viewDidLoad()
+        
         self.createWebview()
         NotificationCenter.default.addObserver(self, selector: #selector(self.pushReceiver), name: Notification.Name("fcm_data"), object: nil)
     }
@@ -37,6 +40,36 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
         self.createMainview(config: config)
         self.createBannerview(config: config)
         self.startWebview()
+    }
+    
+    
+    func createSplash(){
+        let manager = SwiftyGifManager(memoryLimit: 20)
+        let image = UIImage(gifName: "intro")
+        self.splash = UIImageView(gifImage: gifImage, manager: gifManager, loopCount: 1)
+        self.splash.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        self.splash.center = self.view.center
+        self.splash.contentMode = UIViewContentMode.scaleAspectFit
+        self.splash.delegate = self
+        self.splash.backgroundColor = UIColor(red: 0.11, green: 0.25, blue: 0.13, alpha:1.0)
+        self.view.addSubview(self.splash)
+//        self.splashBackground = UIImageView(frame: self.view.frame)
+//        self.splashBackground.backgroundColor = UIColor(red: 0.11, green: 0.25, blue: 0.13, alpha:1.0)
+//        self.view.addSubview(splashBackground)
+//        self.view.addSubview(self.splashScreen)
+        
+    }
+    
+    
+    func removeSplash(){
+        UIView.animate(withDuration: 0.5, animations: {
+            self.splash.alpha = 0
+            self.webView.alpha = 1
+        }, completion: { finished in
+            self.splash.removeFromSuperview()
+            self.statusBarHidden = false
+            self.setNeedsStatusBarAppearanceUpdate()
+        })
     }
     
     /** 기본 웹뷰 초기설정 및 만들기 **/
@@ -57,17 +90,20 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
         banner.backgroundColor = UIColor(rgb: 0x1C3F21)
         banner.load(URLRequest(url: URL(string: SITE_BANNER)!))
         self.view.addSubview(banner)
-//        self.bannerAnimation(fadeIn: true)
     }
     
     /** 웹뷰 컨트롤러 만들기 **/
     func createWebviewController() -> WKUserContentController{
-        let js = "javascript: localStorage.setItem('key', 'value')";
-        let userScript = WKUserScript(source: js, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false);
         let contentController = WKUserContentController()
         contentController.add(self, name: "callbackHandler")
-        contentController.addUserScript(userScript)
+        contentController.addUserScript(self.getUserScript(script: "javascript: localStorage.setItem('key', 'value')"))
+        contentController.addUserScript(self.getUserScript(script: "javascript: sessionStorage.setItem('key', 'value')"))
         return contentController
+    }
+    
+    /** 웹뷰 컨틀롤러에 추가될 스크립트 **/
+    func getUserScript(script: String) -> WKUserScript{
+        return WKUserScript(source: script, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false)
     }
     
     /** 기본 웹뷰의 시작 페이지 불러오기 **/
@@ -229,7 +265,6 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
         }
     }
     
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -244,6 +279,13 @@ extension UIColor{
             blue: CGFloat(rgb & 0x0000FF) / 255.0,
             alpha: CGFloat(1.0)
         )
+    }
+}
+
+/** 스플래시 애니메이션 완료 시. **/
+extension ViewController: SwiftyGifDelegate {
+    func gifDidLoop() {
+        self.removeSplashScreen()
     }
 }
 
