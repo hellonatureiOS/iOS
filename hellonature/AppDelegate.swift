17a1f4228de6bcc7cc1f7953b3e22736534555a0
Daesung Jeon <daesungjeon@daesungui-macbook-pro.local>
2018-12-14 18:38:58 -0800

    commit test

--- hellonature/AppDelegate.swift
+++ hellonature/AppDelegate.swift
@@ -17,86 +17,109 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     var viewController: ViewController?
     var sharedData: [String : String] = [:]
     let gcmMessageIDKey = "gcm.message_id"
-
+    private var splash : UIImageView!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         debugPrint("@1 AppDelegate DidFinishLaunchingWithOptions")
+        print("===1")
         if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().delegate = self
         }
         self.registerForPushNotifications()
         self.window!.rootViewController?.view.backgroundColor = UIColor.white
         self.viewController = self.window?.rootViewController as? ViewController
+        
+        print("@forground")
+        
+        
         return true
     }
-
-
+   
+    
+    
+    
     /** 앱이 백그라운드에서 알림 메시지를받는 경우 **/
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        print("===2")
         Messaging.messaging().appDidReceiveMessage(userInfo)
         if let messageID = userInfo[gcmMessageIDKey] {
             debugPrint("@3 Message ID: \(messageID)")
         }
     }
-
+    
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("===3")
         if let messageID = userInfo[gcmMessageIDKey] {
             debugPrint("@4 Message ID: \(messageID)")
             Messaging.messaging().appDidReceiveMessage(userInfo)
             completionHandler(UIBackgroundFetchResult.newData)
         }
     }
-
+    
     /** 알림 등록실패 **/
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error){
         debugPrint("@5 didFailToRegisterForRemoteNotificationsWithError: \(error)")
+        
     }
-
+    
     /** 디바이스토큰 등록완료 후 호출 **/
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)
     {
+        print("===5")
+       
         let token = String(format: "%@", deviceToken as CVarArg)
         Messaging.messaging().apnsToken = deviceToken
         debugPrint("@6 deviceToken: \(token)")
+        
     }
     
     /** auto constraints fix **/
     func application(_ application: UIApplication, willChangeStatusBarFrame newStatusBarFrame: CGRect) {
+        print("===6")
+        
         let windows = UIApplication.shared.windows
         for window in windows {
             window.removeConstraints(window.constraints)
         }
     }
     
-
+    
     func application(received remoteMessage: MessagingRemoteMessage)
     {
+        print("===8")
         debugPrint("@7 remoteMessage:\(remoteMessage.appData)")
     }
     
     /** 앱 포그라운드 **/
     func applicationWillEnterForeground(_ application: UIApplication) {
+      print("===9")
     }
     
     /** 앱 백그라운드 **/
     func applicationDidEnterBackground(_ application: UIApplication) {
+        print("===10")
         Messaging.messaging().shouldEstablishDirectChannel = false
         debugPrint("@8 AppDelegate DidEnterBackground")
     }
     
     /** 앱 활성화 **/
     func applicationDidBecomeActive(_ application: UIApplication) {
+        print("===11")
         Messaging.messaging().shouldEstablishDirectChannel = true
         debugPrint("@9 AppDelegate DidBecomeActive")
+        
     }
     
     /** 앱 종료 **/
     func applicationWillTerminate(_ application: UIApplication) {
     }
-
+    
     /** FCM 등록 **/
     func registerForPushNotifications()
     {
+       print("===13")
         debugPrint("@10 registerForPushNotifications")
+        print("?????")
         // iOS 10에서 알림제어
         if #available(iOS 10.0, *)
         {
@@ -121,15 +144,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             let setting = UIUserNotificationSettings(types: type, categories: nil);
             UIApplication.shared.registerUserNotificationSettings(setting);
         }
-
+        
         FirebaseApp.configure()
         Messaging.messaging().delegate = self
         Messaging.messaging().shouldEstablishDirectChannel = true
         UIApplication.shared.registerForRemoteNotifications();
+        
+        
+        
     }
-
+    
     func tokenRefreshNotificaiton(_ notification: Foundation.Notification)
     {
+        print("===14")
         if let refreshedToken = InstanceID.instanceID().token()
         {
             debugPrint("@12 InstanceID token: \(refreshedToken)")
@@ -138,6 +165,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     /** 알림 데이터 전송 **/
     func setSharedData(userInfo: [AnyHashable: Any], send: Bool){
+        print("===15")
         print("@@@@\(userInfo)")
         guard let pushNo = userInfo["push_no"] else {
             return
@@ -152,17 +180,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             NotificationCenter.default.post(name: Notification.Name("fcm_data"), object: nil)
         }
     }
-
-
+    
+    
     /** 새로운 FCM 토큰을 받을 때마다 호출 **/
     func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String)
     {
+        print("===16")
         NotificationCenter.default.post(name: Notification.Name("device_token"), object: nil)
         debugPrint("@13 didRefreshRegistrationToken:\(fcmToken)")
     }
     @available(iOS 10.0, *)
     public func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage)
     {
+        print("===17")
         debugPrint("@14 messaging:\(messaging)")
         debugPrint("@14 didReceive Remote Message:\(remoteMessage.appData)")
         guard let data =
@@ -175,6 +205,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     @available(iOS 10.0, *)
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
     {
+       print("===18")
+        print("=========")
+        print("=========")
+        print("=========")
+        print("=========")
+        print("=========")
+        
         //알림 처리
         self.setSharedData(userInfo: notification.request.content.userInfo, send: false);
         debugPrint("@15 notification: \(notification)")
@@ -186,6 +223,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     @available(iOS 10.0, *)
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void)
     {
+        print("===19")
         //알림 처리
         self.setSharedData(userInfo: response.notification.request.content.userInfo, send: true);
         debugPrint("@16 didReceive response Notification \(response) ")
@@ -195,6 +233,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     /** 딥링크 **/
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        //self.splashDidShow()
         // 카카오링크
         if KLKTalkLinkCenter.shared().isTalkLinkCallback(url) {
             sharedData["kakaolink"] = url.query
@@ -213,17 +252,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     /** 파이어베이스 다이나믹 링크 **/
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        print("===21")
         let handled = DynamicLinks.dynamicLinks()?.handleUniversalLink(userActivity.webpageURL!) { (dynamiclink, error) in
             if let link = dynamiclink!.url {
                 NotificationCenter.default.post(name: Notification.Name("dynamic_link"), object: nil, userInfo: ["link" : String(describing: link)])
             }
         }
-
+        
         return handled!
     }
     
-
+    
     func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+        print("===22")
         if KLKTalkLinkCenter.shared().isTalkLinkCallback(url) {
             sharedData["kakaolink"] = url.query
             let alert = UIAlertController(title: "알림", message: "test" , preferredStyle: .alert)
@@ -241,11 +282,13 @@ extension Notification.Name {
 
 extension MessagingDelegate {
     func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String) {
+        print("===23")
         debugPrint("@17 Firebase registration token: \(fcmToken)")
     }
     // 앱이 포 그라운드 일 때 FCM (APN 무시)에서 직접 iOS 10 이상의 데이터 메시지를받습니다.
     // 직접적인 데이터 메시지를 활성화하려면 Messaging.messaging (). shouldEstablishDirectChannel을 true로 설정합니다.
     func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        print("===24")
         print("@18 Received data message: \(remoteMessage.appData)")
     }
 }
--- hellonature/ViewController.swift
+++ hellonature/ViewController.swift
@@ -10,6 +10,7 @@ import FirebaseMessaging
 import SwiftyGif
 import EasyAnimation
 import UserNotifications
+import MessageUI
 
 // PG사 도메인
 enum Billing: String, RawRepresentable {
@@ -30,6 +31,8 @@ enum Blank: String, RawRepresentable {
     case Naverblog = "blog.naver.com"
     case Facebook = "ko-kr.facebook.com"
     case Instagram = "www.instagram.com"
+    case okCashBack = "www.okcashbag.com"
+    case FacebookLogin = "www.facebook.com"
 }
 
 // 스키마 모음
@@ -42,6 +45,14 @@ enum Scheme: String, RawRepresentable {
     case Kakaotalk = "kakaotalk"
     case Deeplink = "deeplink"
     case Ispmobile = "ispmobile"
+    //앱설치 되지않았을때 카드사별 스키마모음 2018-12-02 by ds
+    case hyundae = "hdcardappcardansimclick"
+    case nonghyup = "nhallonepayansimclick"
+    case Samsungcard = "mpocket.online.ansimclick"
+    case Shinhancard = "shinhan-sr-ansimclick"
+    case Lottecard = "lotteappcard"
+    case Hanacard = "cloudpay"
+    case Citibank = "citimobileapp"
 }
 
 // 헬로네이처
@@ -87,7 +98,7 @@ enum Bridge: String {
     case SettingsInfo = "push permission info"
 }
 
-class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKScriptMessageHandler{
+class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKScriptMessageHandler,MFMessageComposeViewControllerDelegate{
     var webView: WKWebView!
     var banner: WKWebView!
     var splash: UIView!
@@ -96,7 +107,9 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
     @objc var showStatusBar:Bool = false
     var screenSize:CGRect!
     var uagt: String?
-    
+    var window: UIWindow?
+    var splash1 : UIImageView!
+    private var contentView: UIView?
     /** 뷰컨트롤러 시작 **/
     override func viewDidLoad() {
         EasyAnimation.enable()
@@ -105,8 +118,13 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
         self.createWebview()
         self.createSplash()
         self.createToolbar()
+
         self.mainView = self.webView
         self.view.backgroundColor = UIColor.white
+        
+        /** 2018-11-14스플래시 웹뷰 옵저버 추가 by ds **/
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: NSKeyValueObservingOptions.new, context: nil)
         
         UIApplication.shared.statusBarView?.backgroundColor = UIColor.white
         NotificationCenter.default.addObserver(self, selector: #selector(self.applicationDidBecomeActive), name: NSNotification.Name.UIApplicationDidBecomeActive, object: nil)
@@ -115,16 +133,49 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
         NotificationCenter.default.addObserver(self, selector: #selector(self.linkReceiver), name: Notification.Name("kakao_link"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.linkReceiver), name: Notification.Name("dynamic_link"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.tokenReceiver), name: Notification.Name("device_token"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
+       
     }
+    /**웹뷰에서 키보드활성화될때 호출 아직 기능없음***/
+    @objc func keyboardWillShow(notification: NSNotification) {
+        print("keyboardWillShow")
+       
+    }
+
+    /** 2018-11-14스플래시 옵저버 기능 by ds
+     1.isLoading : 웹뷰 로딩이 완료되는 시점 알수 있음.
+     2.estimatedProgress : 관제 기능 현재 URL 찾기
+     **/
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        print(self.webView.isLoading)
+        if !self.webView.isLoading{
+            print("====false=====")
+            
+            UIApplication.shared.isStatusBarHidden = false
     
+            self.splashDidRemove()
+            
+        }
+        /*if (keyPath == "estimatedProgress")
+        {
+        
+        }*/
+    }
     /** 앱 활성화될 때 **/
     @objc func applicationDidBecomeActive(_ notification: NSNotification?) {
         // 웹뷰가 만들어졌다면 웹뷰에 알림등록 상태를 전달한다.
+        
+        
         if webView != nil {
             self.sendRegisteredForNotifications()
         }
         // 앱델리게이트에서 푸시, 카카오링크, 딥링크등의 정보를 얻어와서 해당링크로 이동시킨다.
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+      
+        
+        //print(appDelegate.sharedData[Scheme.Deeplink.rawValue])
         if let pushNo = appDelegate.sharedData["pushno"] {
             self.httpRequest("\(Push.Update.url)\(pushNo)", parameters: ["uid": self.getDeviceToken()] as AnyObject)
             guard let startURL = appDelegate.sharedData["pushlink"], startURL.count > 0 else {
@@ -214,12 +265,11 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
             }
         }
     }
-    
-    /** 스플래시 애니메이션 붙이기 **/
+    /** 스플래시 애니메이션 붙이기 GIF**/
     func createSplash(){
-        let gifManager = SwiftyGifManager(memoryLimit: 20)
+        /*let gifManager = SwiftyGifManager(memoryLimit: 20)
         let gifImage = UIImage(gifName: "intro")
-        let gifView = UIImageView(gifImage: gifImage, manager: gifManager, loopCount: 1)
+        let gifView = UIImageView(gifImage: gifImage, manager: gifManager, loopCount: 100)
         gifView.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
         gifView.center = self.view.center
         gifView.contentMode = UIViewContentMode.scaleAspectFit
@@ -228,9 +278,75 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
         self.splash.addSubview(gifView)
         self.splash.backgroundColor = UIColor(red: 0.11, green: 0.25, blue: 0.13, alpha:1.0)
         self.view.addSubview(self.splash)
+        self.showStatusBar = false*/
+        
+         /** 2018-11-14스플래시 gif -> png 교체  by ds**/
+         //let testImage = UIImage(named: self.getSplashImageName())
+        let testImage = UIImage(named: "splashimage")
+        let testView = UIImageView(image: testImage)
+        testView.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
+        testView.center = self.view.center
+        testView.contentMode = UIViewContentMode.scaleAspectFit
+        testView.delegate = self
+        
+        self.splash = UIView(frame: self.view.frame)
+        self.splash.addSubview(testView)
+        self.splash.backgroundColor = UIColor(red: 0.11, green: 0.25, blue: 0.13, alpha:1.0)
+        self.view.addSubview(self.splash)
         self.showStatusBar = false
+       
     }
+    /** 2018-11-14스플래시 이미지 리사이징 by ds**/
     
+    func getSplashImageName() -> String
+    {
+        let viewSize = UIScreen.main.bounds.size
+        
+        guard let imagesDict = Bundle.main.infoDictionary as [String: AnyObject]?,
+        let ImagesArray = imagesDict["UILaunchImages"] as? [[String:String]]else
+        {
+            return "test6"
+        }
+        var viewOrientation:String
+        switch UIDevice.current.orientation
+        {
+            case .portrait:
+                viewOrientation = "Portrait"
+            case .portraitUpsideDown :
+                viewOrientation = "PortraitUpsideDown"
+            default:
+                viewOrientation = "Portrait"
+        }
+        for dict in ImagesArray
+        {
+            if let sizeString = dict["UILaungchImageSize"], let imageOrientation = dict["UILaunchImageOrientation"]
+            {
+                let imagesSize = CGSizeFromString(sizeString)
+                if imagesSize.equalTo(viewSize) && viewOrientation == imageOrientation
+                {
+                    if let imageName = dict["UILaunchImageName"]
+                    {
+                        return imageName
+                    }
+                }
+            }
+        }
+        return "test6"
+    }
+     /** 2018-11-14스플래시 애니메이션 삭제  by ds**/
+    func splashDidRemove()
+    {
+        
+        print(#function)
+        UIView.animate(withDuration: 0.4,
+                       animations: {self.splash.alpha = 0},
+                       completion: {_ in
+                        self.splash.removeFromSuperview()
+                        UIApplication.shared.isStatusBarHidden = false;
+                        
+        })
+        
+    }
     /** 스플래시 애니메이션 삭제 **/
     func removeSplash(){
         self.tween(current: self.splash, next: self.mainView!)
@@ -256,7 +372,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
     /** 배너뷰 초기설정 및 만들기 **/
     func createBannerview(config: WKWebViewConfiguration){
         banner = WKWebView(frame: CGRect(x: 0, y: 0, width: self.screenSize.width, height: self.screenSize.height), configuration: config)
-        banner.navigationDelegate = self
+        //banner.navigationDelegate = self
         banner.uiDelegate = self
         banner.scrollView.isScrollEnabled = false
         banner.scrollView.bounces = false
@@ -279,23 +395,50 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
     func getUserScript(script: String) -> WKUserScript{
         return WKUserScript(source: script, injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false)
     }
+    //SMS 전송 추가 by ds 2018-12-13
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+    }
     
     // 웹뷰 보안 체크
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+       //이니시스 관련 cookie설정 및 쿠키 항상 허용 by ds 2018-11-30
+        let configuration = URLSessionConfiguration.default
+        configuration.httpCookieStorage = HTTPCookieStorage.shared
+        configuration.httpCookieAcceptPolicy = .always
+        configuration.httpShouldSetCookies = false
+        
         guard let url = navigationAction.request.url else {
             decisionHandler(.cancel)
             return
         }
         print("@@@\(url)")
+        print(Scheme.values)
+        print(url.scheme)
+
+        if(url.absoluteString.contains("sms:&body="))
+        {
+            let messageComposeVC = MFMessageComposeViewController()
+            messageComposeVC.messageComposeDelegate = self  //  Make sure to set this property to self, so that the controller can be dismissed!
+        
+            let s:String = String(describing: url)
+            let start = s.replacingOccurrences(of: "sms:&body=", with: "",
+                                               options: NSString.CompareOptions.literal, range:nil)
+            
+            messageComposeVC.body =  start
+            present(messageComposeVC, animated: true, completion: nil)
+           
+        }
         // 아이튠즈는 기본적으로 사파리로 이동
         if url.absoluteString.contains(Blank.Itunes.rawValue) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
             decisionHandler(.cancel)
             return
         }
-        
-        // scheme 형태로 호출할 경우
-        if !Scheme.values.contains(url.scheme ?? "") {
+       
+        //if !Scheme.values.contains(url.scheme ?? "") {
+        if(!url.absoluteString.contains("https://") && !url.absoluteString.contains("http://") && !url.absoluteString.contains("about")){
             // 앱이 설치되어 있는 경우
             if UIApplication.shared.canOpenURL(url) {
                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
@@ -305,12 +448,20 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
             } else {
                 var warnning: (message: String, url: String)?
                 if let scheme = url.scheme {
+          
                     switch scheme {
                         //카카오 톡일 경우
                         case Scheme.Kakaotalk.rawValue: warnning = (message: "카카오톡 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/kakaotalk/id362057947?mt=8")
                         //이니시스 결제
                         case Scheme.Ispmobile.rawValue: warnning = (message: "ISP/페이북 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/isp-%ED%8E%98%EC%9D%B4%EB%B6%81/id369125087?mt=8")
                         // 그외
+                        case Scheme.hyundae.rawValue: warnning = (message: " 현대카드 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/%ED%98%84%EB%8C%80%EC%B9%B4%EB%93%9C-%EC%95%B1%EC%B9%B4%EB%93%9C/id702653088?mt=8")
+                        case Scheme.nonghyup.rawValue: warnning = (message: " 올원페이 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/%EC%98%AC%EC%9B%90%ED%8E%98%EC%9D%B4-%EC%95%B1%EC%B9%B4%EB%93%9C/id1177889176?mt=8")
+                        case Scheme.Samsungcard.rawValue: warnning = (message: " 삼성앱카드가 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/%EC%82%BC%EC%84%B1%EC%95%B1%EC%B9%B4%EB%93%9C/id535125356?mt=8")
+                        case Scheme.Shinhancard.rawValue: warnning = (message: " 신한페이판 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/%EC%8B%A0%ED%95%9C%ED%8E%98%EC%9D%B4%ED%8C%90/id572462317?mt=8")
+                        case Scheme.Lottecard.rawValue: warnning = (message: " 롯데카드 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/%EB%A1%AF%EB%8D%B0%EC%B9%B4%EB%93%9C-%EC%95%B1%EC%B9%B4%EB%93%9C/id688047200?mt=8")
+                        case Scheme.Hanacard.rawValue: warnning = (message: " 하나1Q페이 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/%ED%95%98%EB%82%981q%ED%8E%98%EC%9D%B4/id847268987?mt=8")
+                        case Scheme.Citibank.rawValue: warnning = (message: " 씨티모바일 앱이 설치되지 않았습니다.", url: "https://itunes.apple.com/kr/app/%EC%94%A8%ED%8B%B0%EB%AA%A8%EB%B0%94%EC%9D%BC/id1179759666?mt=8")
                         default: warnning = nil
                     }
                 }
@@ -355,13 +506,47 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
     
     // 팝업
     func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        print("initHost======")
         guard let url = navigationAction.request.url, let host = url.host else {
+            print("@@@host")
+            print(navigationAction.request.url)
+            
+            print("@@@host")
             return nil
         }
+       
         // 팝업이 허용된 도메인들
         if navigationAction.targetFrame == nil {
+            print("@@@host2")
+            print(host)
+            
+            print("@@@host2")
+        
+            var isPopUp:Bool = false
+            switch host {
+            // 앱 배너 열기
+            case Blank.Itunes.rawValue:
+                 isPopUp = true
+            // 앱 배너 닫기, 페이지 이동
+            case Blank.Kakaostory.rawValue:
+                 isPopUp = true
+            // 앱 알림설정
+            case Blank.Naverblog.rawValue:
+                 isPopUp = true
+            case Blank.Facebook.rawValue:
+                 isPopUp = true
+            case Blank.Instagram.rawValue:
+                isPopUp = true
+            case Blank.okCashBack.rawValue:
+                isPopUp = true
+            case Blank.FacebookLogin.rawValue:
+                isPopUp = true
+            default:
+                 isPopUp = false
+            }
             // 사파리로 팝업
-            if Blank.values.contains(host) {
+            if (isPopUp){
+            //if Blank.values.contains(host) {
                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
                 return nil
             }
@@ -373,7 +558,10 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
  
     /** 페이지 로딩 완료, webView의 페이지가 로드가 완료 & 처음 페이지 로드시에만 js함수 호출 **/
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("111111111111111111111")
         if webView == self.webView && !self.webViewStarted{
+            print("2222222222222222222")
+            
             self.webViewStarted = true
             self.banner.evaluateJavaScript("c")
         }
@@ -481,7 +669,10 @@ extension ViewController {
     @objc func pushReceiver(_ notification: NSNotification?){
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         if let pushNo = appDelegate.sharedData["pushno"] {
+            print("====PUSHNO ="+pushNo)
+            
             self.httpRequest("https://push.hellonature.net/push/update/\(pushNo)", parameters: ["uid": self.getDeviceToken()] as AnyObject)
+            print("====token ="+self.getDeviceToken())
             guard let startURL = appDelegate.sharedData["pushlink"], startURL.count > 0 else {
                 return
             }
